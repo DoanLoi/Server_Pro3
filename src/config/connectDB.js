1788b67acda7693d2e80449f77a0dfe42a2253e2
DoanLoi <chomavai@gmail.com>
2020-12-27 17:25:43 +0700

    aa

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -5,7 +5,7 @@ console.log("====", process.env.MONGODB_URI);
 let connectDB = () => {
   mongoose.Promise = bluebird;
   try {
-    mongoose.connect(process.env.MONGODB_URI+'', {
+    mongoose.connect('mongodb+srv://admin:admin@cluster0.livbm.mongodb.net/HomeLynh?authSource=admin&replicaSet=atlas-ot4snx-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true&retryWrites=false', {
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true,
